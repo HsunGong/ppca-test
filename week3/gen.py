33367750139856e5b16db015921d6a3037f099c7
xun <gongxuncd@gmail.com>
2019-07-13 23:30:13 +0800

    little change of tests

--- week3/gen.py
+++ week3/gen.py
@@ -1,6 +1,8 @@
 from random import shuffle
 import numpy
 
+
+## False
 # k = [5, 10, 12]
 # for i in range(0, 3):
 #     with open("./data/false" + str(i) + ".in", "w") as f:
@@ -15,12 +17,33 @@ import numpy
 #             f.write(' ')
 #         f.write('\n')
 
+
+## True
+# k = [5, 8, 9, 10, 11, 11, 12]
+# for i in range(len(k)):
+#     with open("./data/true" + str(i) + ".in", "w") as f:
+#         # arr = numpy.random.randint(low=0, high=1<<k[i], size=1<<k[i])
+#         arr = numpy.arange(0, 1<<k[i])
+#         shuffle(arr)
+
+#         f.write(str(k[i]))
+#         f.write('\n')
+#         for a in arr:
+#             f.write(str(a))
+#             f.write(' ')
+#         f.write('\n')
+
+## True-2
 k = [5, 8, 9, 10, 11, 11, 12]
 for i in range(len(k)):
     with open("./data/true" + str(i) + ".in", "w") as f:
         # arr = numpy.random.randint(low=0, high=1<<k[i], size=1<<k[i])
-        arr = numpy.arange(0, 1<<k[i])
-        shuffle(arr)
+        p = numpy.arange(0, 1<<k[i])
+        shuffle(p)
+        q = numpy.arange(0, 1<<k[i])
+        shuffle(q)
+
+        arr = [p[m]^q[m] for m in range(len(p))]
 
         f.write(str(k[i]))
         f.write('\n')
