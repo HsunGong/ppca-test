9bb75dacd9f6f99132642332aa0b35c5c777277f
xun <gongxuncd@gmail.com>
2019-07-24 16:03:12 +0800

    backup all times

--- week4/q3/origin/tmp.py
+++ week4/q3/origin/tmp.py
@@ -4,21 +4,29 @@ ans = []
 
 x = 1
 while x < 1e9:
-    ans.append([x, 6])
+    ans.append([x, 2])
     cnt += 1
-    x *= 6
+    x *= 2
 
-x = 36
+x = 2
 while x < 1e9:
-    ans.append([x, 36])
+    ans.append([x, 4])
     cnt += 1
-    x *= 6
+    x *= 4
 
-x=6
+x = 8
 while x < 1e9:
-    ans.append([x, 12])
+    ans.append([x, 16])
     cnt += 1
-    x *= 6
+    x *= 16
+
+x = 128
+while x < 1e9:
+    ans.append([x, 256])
+    cnt += 1
+    x *= 256
+
+
 
 print(cnt)
 for a in ans:
